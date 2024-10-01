ab20c918465cb8558ac40db39fb5d1520163aad2
Diego Castillo <diegocasmo@gmail.com>
2024-09-30 20:24:54 -0500

    Implement simple sign in with magic link and resend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SignIn } from "@/components/SignIn";
+
 export default function Home() {
-  return "testing";
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Magic Link Auth</h1>
+      <SignIn />
+    </div>
+  );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import Resend from "next-auth/providers/resend";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Resend({
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+});
