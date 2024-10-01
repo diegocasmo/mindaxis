8aa8601cb65045007c2c1402c8748e276eb0126b
Diego Castillo <diegocasmo@gmail.com>
2024-09-30 20:37:19 -0500

    Log user email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { SignIn } from "@/components/SignIn";
+import { auth } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-4">Welcome to Magic Link Auth</h1>
-      <SignIn />
+      {session && session.user ? (
+        <p>Welcome, {session.user.email}!</p>
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
 }
