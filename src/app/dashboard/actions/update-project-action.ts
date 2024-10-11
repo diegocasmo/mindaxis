62ba1b513ba0483202b6f1ea2d3885cfe2714262
Diego Castillo <diegocasmo@gmail.com>
2024-10-10 20:31:58 -0500

    Use better variable name

--- src/app/dashboard/actions/update-project-action.ts
+++ src/app/dashboard/actions/update-project-action.ts
@@ -14,7 +14,7 @@ type ActionResult<T> =
   | { success: false; errors: FieldErrors<UpdateProjectSchema> };
 
 export async function updateProjectAction(
-  id: string,
+  projectId: string,
   formData: FormData
 ): Promise<ActionResult<Project>> {
   const result = updateProjectSchema.safeParse({
@@ -32,7 +32,7 @@ export async function updateProjectAction(
     }
 
     const project = await updateProject({
-      projectId: id,
+      projectId: projectId,
       name: result.data.name,
       userId: session.user.id,
     });
